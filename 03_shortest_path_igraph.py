c2eb53069b5d9df907c635c81691c9f118e8a801
Junwon Choi <cjunwon.tech@gmail.com>
2024-05-31 15:10:33 -0700

    Add batch processing

--- 03_shortest_path_igraph.py
+++ 03_shortest_path_igraph.py
@@ -44,9 +44,9 @@ del Node_to_Node_pairs
 
 # print("Subsetting origin_nodes_list...")
 # origin_nodes_list = list(Node_to_Node_pairs_dict.keys())
-
 # random.seed(123)
-# origin_nodes_list_subset = random.sample(origin_nodes_list, 50)
+# origin_nodes_list_subset = random.sample(origin_nodes_list, 51)
+# del origin_nodes_list
 
 print("Creating Node_to_Node_pairs_dict_subset...")
 # Node_to_Node_pairs_dict_subset = {k: v for k, v in Node_to_Node_pairs_dict.items() if k in origin_nodes_list_subset}
@@ -58,24 +58,41 @@ total_subset_pairs = 0
 for key, value in Node_to_Node_pairs_dict_subset.items():
     total_subset_pairs += len(value)
 
+def chunks(data, SIZE=10000):
+    it = iter(data)
+    for i in range(0, len(data), SIZE):
+        if isinstance(data, dict):
+            yield {k: data[k] for k in list(it)[i:i + SIZE]}
+        else:
+            yield data[i:i + SIZE]
+
+SIZE = 1000
+
 print("Using single source dijkstra algorithm in igraph to find shortest paths for ", total_subset_pairs, "pairs...")
+
+print("Creating chunks of Node_to_Node_pairs_dict_subset...")
+Node_to_Node_pairs_dict_subset_chunks = list(chunks(Node_to_Node_pairs_dict_subset, SIZE))
+
+print("Number of chunks to process: ", len(Node_to_Node_pairs_dict_subset_chunks))
 
 time_start = time.time()
 
 shortest_path_results = {}
 
-for origin, destinations in Node_to_Node_pairs_dict_subset.items():
-    shortest_paths = g.get_shortest_paths(origin, to=destinations, weights='weight', output="vpath", algorithm="dijkstra")
-    to_destination_dict = {}
-    for destination, path in zip(destinations, shortest_paths):
-        to_destination_dict[destination] = path
+for i, chunk in enumerate(Node_to_Node_pairs_dict_subset_chunks):
+    print(f"Processing chunk {i+1}/{len(Node_to_Node_pairs_dict_subset_chunks)}...")
+    for origin, destinations in chunk.items():
+        shortest_paths = g.get_shortest_paths(origin, to=destinations, weights='weight', output="vpath", algorithm="dijkstra")
+        to_destination_dict = {}
+        for destination, path in zip(destinations, shortest_paths):
+            to_destination_dict[destination] = path
 
-    shortest_path_results[origin] = to_destination_dict
+        shortest_path_results[origin] = to_destination_dict
 
 time_end = time.time()
 
 print("Time taken for Single Source Method: ", (time_end - time_start)/60, "minutes")
-print("Estimated time for all pairs: ", (time_end - time_start)/60 * Node_to_Node_pairs_len/total_subset_pairs / 60, "hours")
+# print("Estimated time for all pairs: ", (time_end - time_start)/60 * Node_to_Node_pairs_len/total_subset_pairs / 60, "hours")
 
 print("Converting shortest_path_results to tuple keys...")
 
